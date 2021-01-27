31886c0470703613e26a89e781ded8106c904d08
kaitlinkenney <kaitlin.kenney96@gmail.com>
2021-01-26 21:58:05 -0500

    env vars updated

--- Solved/server.js
+++ Solved/server.js
@@ -12,11 +12,20 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
 // Add routes, both API and view
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb+srv://kaitlinkenney:password123!@cluster0.wxtar.mongodb.net/readinglist?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // Start the API server
 app.listen(PORT, function() {
