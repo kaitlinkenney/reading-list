25ccdfda0ae98abacb21386985bc164d223647e4
kaitlinkenney <kaitlin.kenney96@gmail.com>
2021-01-27 13:08:15 -0500

    local db name updated

--- Solved/server.js
+++ Solved/server.js
@@ -18,7 +18,7 @@ app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/readinglist',
+  process.env.MONGODB_URI || 'mongodb://localhost/reactreadinglist',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
